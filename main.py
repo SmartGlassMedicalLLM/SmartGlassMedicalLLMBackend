096a2f42488206412c5bc8952e01a0af14d10bc1
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-02-22 23:23:46 -0500

    Prepares API for base, in-context, fine-tuned

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from google import genai
 import requests
 
 app = FastAPI()
 
-class Question(BaseModel):
+class Input(BaseModel):
     prompt: str
 
-@app.post("/local")
-async def prompt_local_llm(question: Question):
+@app.post("/base")
+async def prompt_base(input: Input):
     response = requests.post(
         "http://localhost:11434/api/generate",
         json={
             "model": "llama3",
-            "prompt": question.prompt,
+            "prompt": input.prompt,
             "stream": False
         }
     )
     
     return response.json()
 
-@app.post("/gemini")
-async def prompt_gemini_llm(question: Question):
-    client = genai.Client()
+@app.post("/in-context")
+async def prompt_in_context(input: Input):
+    return {}
 
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=question.prompt
-    )
-
-    return response
+@app.post("/fine-tuned")
+async def prompt_fine_tuned(input: Input):
+    return {}
 
 if __name__ == "__main__":
     import uvicorn
