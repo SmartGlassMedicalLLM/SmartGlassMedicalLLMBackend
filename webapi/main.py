b987121be2e78f6f331ad7575e3f943fdc11e975
imm5389-bit <imm5389@psu.edu>
2026-03-23 19:26:22 -0400

    updated main.py

--- webapi/main.py
+++ webapi/main.py
@@ -4,11 +4,15 @@ import requests
 
 from medgemma_base import medgemma_base_prompt
 from medgemma_in_context import extract_interactions_from_drug
+from long_t5_script import summarize
 
 app = FastAPI()
 
 class Input(BaseModel):
     prompt: str
+
+class SummarizeInput(Input):
+    max_words: int
 
 @app.post("/base")
 async def prompt_base(input: Input):
@@ -21,6 +25,11 @@ async def prompt_in_context(input: Input):
 @app.post("/fine-tuned")
 async def prompt_fine_tuned(input: Input):
     return {}
+
+@app.post("/summarize")
+async def prompt_summarize(input: SummarizeInput):
+    result = summarize(input.prompt, max_words=input.max_words)
+    return {"summary": result}
 
 if __name__ == "__main__":
     import uvicorn
