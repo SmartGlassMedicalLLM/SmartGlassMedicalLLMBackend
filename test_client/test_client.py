080dfdf7561f840e4f0ea5c191df6ebf8476e6ff
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-03-23 20:10:47 -0400

    Accepts attribute error when parsing response

--- test_client/test_client.py
+++ test_client/test_client.py
@@ -64,7 +64,7 @@ if prompt := st.chat_input("Ask a medical question..."):
                 try:
                     full_response = response.json().get("response")
                     st.markdown(full_response)
-                except ValueError:
+                except (AttributeError, ValueError):
                     st.markdown(response)
 
                 st.session_state.messages.append({"role": "assistant", "content": full_response})
