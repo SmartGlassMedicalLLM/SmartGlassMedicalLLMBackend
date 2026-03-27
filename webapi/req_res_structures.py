a40335dbe25f1c2595fde3b7ff07966c47de3c05
EarthCow <56940983+EarthCow@users.noreply.github.com>
2026-03-27 15:28:35 -0400

    Adds response structures

--- webapi/req_res_structures.py
+++ webapi/req_res_structures.py
@@ -16,3 +16,26 @@ class BaseRequest(BaseModel):
   currPage: int | None = None
   highlights: list[Highlight] | None = None
   pdf: UploadFile | None = None
+
+class DocumentReference(BaseModel):
+  refId: str
+  page: int
+  label: str
+  quote: str
+  highlightedWord: str
+  confidence: float
+
+class BaseResponse(BaseModel):
+  reqRefId: str
+  resRefId: str
+  answer: str
+  references: list[DocumentReference] | None = None
+
+class APIError(BaseModel):
+  code: str
+  message: str
+
+class ErrorResponse(BaseModel):
+  reqRefId: str
+  resRefId: str
+  error: APIError
